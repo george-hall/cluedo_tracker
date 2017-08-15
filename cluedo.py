8684878e6afb06d971837c6d2c81091266ced1f9
George <dyedpurple@googlemail.com>
2017-08-15 01:11:54 +0100

    Remove some fcns used by the old method

--- cluedo.py
+++ cluedo.py
@@ -9,17 +9,6 @@ def print_probs(probs_dict, num_players, card_enum):
         print str(i) + " " + " ".join((str(x)[2:7] if x not in [0,1] else ('!!!!!' if x == 1 else '00000')) for x in probs_dict[i])
 
 
-def create_probs_dict(num_players):
-    probs_dict = {}
-
-    for i in xrange(num_players + 1):
-        probs_dict[i] = [1/len(SUSPECTS) for x in SUSPECTS] + \
-                        [1/len(ROOMS) for x in ROOMS] + \
-                        [1/len(WEAPONS) for x in WEAPONS]
-
-    return probs_dict
-
-
 def create_card_enum(SUSPECTS, ROOMS, WEAPONS):
     return enumerate(SUSPECTS + ROOMS + WEAPONS)
 
@@ -29,16 +18,6 @@ def error_check_cards_list(user_cards_list):
         if not (0 <= num <= 20):
             print "ERROR: All number must be between 0 and 20"
             sys.exit(1)
-
-
-def player_def_has_card(probs_dict, player_num, card_num):
-    for p in probs_dict.keys():
-        if p == player_num:
-            probs_dict[p][card_num] = 1
-        else:
-            probs_dict[p][card_num] = 0
-
-    return probs_dict
 
 
 def parse_input(user_input):
